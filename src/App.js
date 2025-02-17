919e19a342c336e7fdc642205148d7224108e277
Karthiksai01 <karthiksaibayya24@gmail.com>
2025-02-17 23:05:28 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import BrowseBooks from './pages/BrowseBooks';
+import BookDetailsPage from './pages/BookDetailsPage';
+import AddBookPage from './pages/AddBookPage';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/browse" element={<BrowseBooks />} />
+        <Route path="/books/:category/:id" element={<BookDetailsPage />} />
+        <Route path="/add-book" element={<AddBookPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
